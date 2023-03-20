af7646b0444ec674bd9ea5b2d3ed65d749929f4d
Hugo <hugo@boahstudio.com>
2023-03-20 05:54:51 -0300

    First Commit

---
+++ app.js
@@ -0,0 +1,24 @@
+require('colors')
+
+const { inquirerMenu } = require('./helpers/inquirer')
+
+
+console.clear()
+
+const main = async() => {
+    console.log('Hola Mundo')
+
+    let opt = ''
+    
+    do {
+        opt = await inquirerMenu()
+        console.log({ opt });
+
+    } while( opt !== '0')
+
+    // pausa()
+
+}
+
+
+main()
