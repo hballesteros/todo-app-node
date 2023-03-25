9e1e74ee09519436f2d943d1092228b04685a189
Hugo <hugo@boahstudio.com>
2023-03-24 23:56:31 -0300

    Proyecto terminado

--- app.js
+++ app.js
@@ -1,22 +1,72 @@
 require('colors')
 
-const { inquirerMenu } = require('./helpers/inquirer')
+const { guardarDB, leerDB } = require('./helpers/guardarArchivo')
+const { inquirerMenu, 
+        pausa, 
+        leerInput, 
+        listadoTareasBorrar,
+        confirmar,
+        mostrarListadoChecklist
+} = require('./helpers/inquirer')
 
+const Tareas = require('./models/tareas')
 
-console.clear()
 
 const main = async() => {
-    console.log('Hola Mundo')
 
     let opt = ''
-    
+    const tareas = new Tareas()
+
+    const tareasDB = leerDB()
+
+    if ( tareasDB ) {
+        tareas.cargarTareasFromArray( tareasDB )
+    }
+   
     do {
         opt = await inquirerMenu()
-        console.log({ opt });
+
+        switch (opt) {
+            case '1': // crear tarea
+                const  desc = await leerInput('Descripción:')
+                tareas.crearTarea( desc )
+                break
+        
+            case '2': // lista completa
+                tareas.listadoCompleto()
+                break
+            
+            case '3': // listar completadas
+                tareas.listarPendientesCompletadas(true)
+                break
+            
+            case '4': // listar pendientes
+                tareas.listarPendientesCompletadas(false)
+                break
+            
+            case '5': // completado | pendiente
+                const ids = await mostrarListadoChecklist( tareas.listadoArr )
+                tareas.toggleCompletadas ( ids )
+                break
+            
+            case '6': // Borrar tareas
+                const id = await listadoTareasBorrar( tareas.listadoArr )
+                if ( id !== '0') {
+                    const ok = await confirmar('¿Está seguro?')
+                    if ( ok ) {
+                        tareas.borrarTarea( id ) 
+                        console.log('Tarea borrada');
+                    }
+                }
+                break
+        }
+
+        guardarDB( tareas.listadoArr )
+
+        await pausa()
 
     } while( opt !== '0')
 
-    // pausa()
 
 }
 
